e1a8182f7d3febe683f992a8991eb02c3489f82b
Ashik Hegde <ashikhegde2@gmail.com>
2023-05-10 17:23:01 -0400

    Create Course.java

---
+++ Course.java
@@ -0,0 +1,16 @@
+public class Course {
+    private String courseName;
+    
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+    
+    // Getter and setter methods for course name
+    public String getCourseName() {
+        return courseName;
+    }
+    
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+}
